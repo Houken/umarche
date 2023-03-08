657ae518a220af5a78cea87dab29608f3498e57d
Marucci Fujimaru <houken@me.com>
2023-03-08 15:24:14 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,5 @@
     コンポーネントテスト1
     <x-tests.card title="タイトル1" content="本文1" :variable="$message" />
     <x-tests.card title="タイトルのみ" />
+    <x-tests.card title="cssを変更したい" class="bg-red-50"></x-tests>
 </x-tests.app>
