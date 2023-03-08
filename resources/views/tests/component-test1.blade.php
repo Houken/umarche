da75b9f2c7d4ac03df56aad6f7d0017d87e93635
Marucci Fujimaru <houken@me.com>
2023-03-08 14:59:40 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,5 +3,5 @@
         ヘッダー1
     </x-slot>
     コンポーネントテスト1
-    <x-tests.card title="タイトル1" content="本文1" />
+    <x-tests.card title="タイトル1" content="本文1" :variable="$message" />
 </x-tests.app>
